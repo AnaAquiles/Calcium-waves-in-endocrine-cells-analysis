8f732b07e72afdc587f3798987088b2ff90d8076
Erika Gonzalez <akire_hppmo@ciencias.unam.mx>
2019-01-17 17:13:36 -0600

    Falta pasar un dato de la ventana principal a la ventana de la gráfica

--- GUI5_Principal.py
+++ GUI5_Principal.py
@@ -15,8 +15,9 @@ from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 from PyQt5 import uic
 
-MainWin= uic.loadUiType("C:\\Users\\akire\\Desktop\\GUITHUB_VentanasSeperadas\\GUI5_MainWin.ui")[0]
-FirstDialogWin = uic.loadUiType("C:\\Users\\akire\\Desktop\\GUITHUB_VentanasSeperadas\\GUI5_FirstDialog.ui")[0]
+MainWin= uic.loadUiType("GUI5_MainWin.ui")[0]
+FirstDialogWin = uic.loadUiType("GUI5_FirstDialog.ui")[0]
+SeconDialogWin = uic.loadUiType("GUI5_SecondDialog.ui")[0]
 
 
 #class StackWinClass(QtGui.QMainWindow, StackWin):
@@ -50,8 +51,6 @@ class MainWinClass(QtGui.QMainWindow, MainWin):
     def openFile(self):                                                        #                                         <------Abre un archivo
 #        self.imv1.clear()                                                      # Si se abre otro archivo se limpia la gráfica
 
-
-
         filename = QtGui.QFileDialog.getOpenFileName(self, 'Open File',\
                                                      os.getenv('HOME'))        #Obtiene la ruta del SO
         lista0 = str(filename[0])                                              #Ahora filename regresa el nombre como una tupla, hay que tomar su primera parte 
@@ -69,24 +68,37 @@ class MainWinClass(QtGui.QMainWindow, MainWin):
         
         #Luego hay que preguntar si son neuronas o células de la hipófisis, y luego de qué frame a qué frame se va a realizar la segmentación en una ventana de diálogo
         #Hay que sacar la serie de tiempo del stack
-        SerieTiempo = np.zeros(self.NoFrames)
+        self.SerieTiempo = np.zeros(self.NoFrames)
         for frame in range(self.NoFrames):  
             imagen_i = self.data[frame,:,:] 
-            SerieTiempo[frame] = np.mean(imagen_i)
+            self.SerieTiempo[frame] = np.mean(imagen_i) 
 
 #        plt.plot(SerieTiempo)
 #        plt.show()
 
-        self.FirstDialogWin = FirtsDialogWinClass()
+        self.FirstDialogWin = FirstDialogWinClass()
         
         
         ItemGrafica = pg.PlotCurveItem(pen=(0,255,0))                          #Se hace un ítem de una gráfica  
-        ItemGrafica.setData(SerieTiempo)                                       #Se agregan los datos al ítem
+        ItemGrafica.setData(self.SerieTiempo)                                       #Se agregan los datos al ítem
         self.FirstDialogWin.TimeSeriesPlot.addItem(ItemGrafica)  
 #        self.ChildStackWin.imv1.setImage(self.data, transform=tr)                            #Mostrar el video en la GUI    
 
-        
         self.FirstDialogWin.show()                
+        
+        
+
+#    def radio_button_clicked(self):
+#        print(self.FirstDialogWin.CellTypeGroup.checkedId())
+#        print(self.FirstDialogWin.CellTypeGroup.checkedButton().text())
+        
+        
+#        print('BLA')
+#        if (self.FirstDialogWin.PituitaryButton.isChecked() == True) or (self.FirstDialogWin.NeuronButton.isChecked() == True):
+#            print('Ya eligieron el tipo celular')
+            
+        
+
 
         
 """qt designer modal dialog python
@@ -103,21 +115,53 @@ https://stackoverflow.com/questions/41150669/open-second-window-from-main-with-p
 #        self.myOtherWindow.show()
         
         
-class FirtsDialogWinClass(QtGui.QMainWindow, FirstDialogWin):
+class FirstDialogWinClass(QtGui.QMainWindow, FirstDialogWin):
     def __init__(self, parent = MainWinClass):
-        super(FirtsDialogWinClass, self).__init__()
+        super(FirstDialogWinClass, self).__init__()
         self.setupUi(self)
-        self.setWindowFlags(  
-        QtCore.Qt.FramelessWindowHint |
-        QtCore.Qt.WindowStaysOnTopHint |
-        QtCore.Qt.X11BypassWindowManagerHint)                                       #https://stackoverflow.com/questions/40866883/pyqt5-change-mainwindow-flags
-                                                                               #https://stackoverflow.com/questions/34160160/creating-window-that-has-no-close-button-in-qt
-        self.setWindowModality(Qt.ApplicationModal)                                                                      
+        
+#        #Para que la nueva ventana no se pueda cerrar y esté siempre encima de la ventana principal:
+#        self.setWindowFlags(  
+#        QtCore.Qt.FramelessWindowHint |
+#        QtCore.Qt.WindowStaysOnTopHint )                                       #https://stackoverflow.com/questions/40866883/pyqt5-change-mainwindow-flags
+#                                                                               #https://stackoverflow.com/questions/34160160/creating-window-that-has-no-close-button-in-qt
+#        #Para que la ventana de dialogo no deje que se pueda modificar la ventana principal:                                                                      
+#        self.setWindowModality(QtCore.Qt.ApplicationModal)                     #https://stackoverflow.com/questions/22410663/block-qmainwindow-while-child-widget-is-alive-pyqt                                                 
 
-        #        form_class_1.show()
-#        uic.loadUi("GUI4_PlotTableWin.ui", self).show()
+                               
+                               
+        self.FirstDialogButton.clicked.connect(self.FirstDialog)                      
+        self.partnerDialog = MainWinClass()
+        serie = self.partnerDialog.SerieTiempo   #¿CÓMO PASAR LOS DATOS DE LA VENTANA ANTERIOR A ESTA NUEVA VENTANA?? https://stackoverflow.com/questions/36772927/pyqt-how-to-pass-information-between-windows
+#        print(serie)
+        
+    def FirstDialog(self):
+        frame1 = self.Fr1_spinBox.value();
+        frame2 = self.Fr2_spinBox.value();   
+        print(frame1)                                             
+        print(frame2)              
+#        print(len(self.partnerDialog.SerieTiempo))                       
+        if (frame2 <= frame1) or (frame2 - frame1 < 300):
+            
+            self.SecondDialogWin = SecondDialogWinClass()
+            self.SecondDialogWin.show()                                    
+     #fALTA LEER LOS RADIO BUTTONS                          
+                               
+#        self.PituitaryButton.clicked.connect(self.radio_button_clicked) 
+#        self.NeuronButton.clicked.connect(self.radio_button_clicked) 
+#        
+#    def radio_button_clicked(self):
+#        print(self.CellTypeGroup.checkedId())
+#        print(self.CellTypeGroup.checkedButton().text())        
 
-
+        
+class SecondDialogWinClass(QtGui.QMainWindow, SeconDialogWin):
+    def __init__(self, parent = FirstDialogWinClass):
+        super(SecondDialogWinClass, self).__init__()
+        self.setupUi(self)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint )    
+        
+        
         
 app = QtGui.QApplication(sys.argv)
 MyWindow = MainWinClass(None)
