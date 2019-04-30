8ddd31639054a495f3f2f630d95836b89f3ba1e0
Erika Gonzalez <akire_hppmo@ciencias.unam.mx>
2019-04-30 15:03:42 -0500

    Subiendo la parte de Ame

--- GUI5_Pituitary.py
+++ GUI5_Pituitary.py
@@ -14,11 +14,16 @@ import matplotlib.pyplot as plt
 
 
 
+def Clasification(serie):
+    print('H')
+    
+    
 def PituitarySegm(frame0, frame1, CellRad, data):
     #abs(F - Fmean)/Fmean para aplanarla 
     #buscar la forma de normalizarla en el dominio!!!
 
-    (NoFrames, alto, ancho) = data.shape()
+    #Información de los datos
+    (NoFrames, alto, ancho) = data.shape
     DiameterDict = {3:1, 5:2, 7:2.5, 9:3, 11:4, 13:4.5}                        #Variación sigma con el diámetro (impar)
     Sigma = DiameterDict[CellRad]                                              #Sigma para el blob
 
@@ -35,31 +40,35 @@ def PituitarySegm(frame0, frame1, CellRad, data):
         y, x, r = blob
         c = plt.Circle((x, y), r, color='yellow', linewidth=1, fill=False)
         axes.add_patch(c)
-                        
+    
     axes.set_axis_off()
     
     plt.tight_layout()
     plt.show()
     
-#    print('Paso 0')
-#    #Clasificación de las series en los spots    
-#    for blob in blobs_log:
-#        binaria = np.zeros((alto, ancho))                                      #Imagen binaria que contendrá la máscara del spot
-#        y, x, r = blob                                                         #Centro y radio del spot
-#        cv2.circle(binaria,(int(x),int(y)), int(r), (255,255,255), -1)         #Creación de la máscara del spot
-#        area = cv2.countNonZero(binaria)                                       #Encontramos el número de pixeles diferentes de cero
-#        coordenadas = np.argwhere(binaria == 255)                              #Buscamos las coordenadas de los pixeles blancos en la imagen binaria
-#        SerieTiempo = np.zeros(NoFrames)                                       #Contendrá la serie de tiempo
-#
-#        for frame in range(NoFrames):                                          
-#            imagen_i = data[frame,:,:]                                         #Recorremos cada frame
-#            suma = 0                                                           #Suma de los pixeles en el frame
-#            for coordenada in coordenadas:                                     #Recorremos cada coordenada
-#                suma = suma + imagen_i[coordenada[0],coordenada[1]]            #Suma de las intensidades
-#            promedio = suma/area                                               #Sacamos el promedio
-#            SerieTiempo[frame] = round(promedio, 2)                            #El promedio se guarda en la serie
-#            
-#                                                                               #Función que hace la clasificación de la serie
+    #Imagen final de la máscara binaria!! con cv2 van a salir cruces y cosas raras
+    mask = np.zeros((alto, ancho))
+    
+    #Generación de las series en los spots    
+    LenSerie = int(frame1-frame0)                                              #Longitud de la serie de tiempo (va a depender de lo que el usuario haya determinado inicialmente)
+    for blob in blobs_log:                                                     #Para cada spot encontrado
+        binaria = np.zeros((alto, ancho))                                      #Imagen binaria que contendrá la máscara del spot
+        y, x, r = blob                                                         #Centro y radio del spot
+        cv2.circle(binaria,(int(x),int(y)), int(r), (255,255,255), -1)         #Creación de la máscara del spot
+        area = cv2.countNonZero(binaria)                                       #Encontramos el número de pixeles diferentes de cero
+        coordenadas = np.argwhere(binaria == 255)                              #Buscamos las coordenadas de los pixeles blancos en la imagen binaria
+        SerieTiempo = np.zeros(LenSerie)                                       #Contendrá la serie de tiempo
+
+        for frame in range(frame0,frame1,1):                                   #Para generar la serie de tiempo de una longitud determinada 
+            imagen_i = data[frame,:,:]                                         #Recorremos cada frame
+            suma = 0                                                           #Suma de los pixeles en el frame
+            for coordenada in coordenadas:                                     #Recorremos cada coordenada
+                suma = suma + imagen_i[coordenada[0],coordenada[1]]            #Suma de las intensidades
+            promedio = suma/area                                               #Sacamos el promedio
+            SerieTiempo[frame] = promedio                                      #El promedio se guarda en la serie        
+        Clasification(SerieTiempo)                                             #Función que hace la clasificación de la serie obtenida
+        
+    
     
     
 #%% 
@@ -105,3 +114,10 @@ def PituitarySegm(frame0, frame1, CellRad, data):
 #    self.Fig_dict = {}                                                     #Diccionario que tendrá las ROIs remarcadas con los checkbox
 #%%
     return (mascara2, ROI_dict)   
+
+
+
+#def Clasification(serie, frame0, frame1, i):
+#    print(i)
+#    
+    
